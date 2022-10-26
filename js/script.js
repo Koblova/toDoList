40ebf2b5116aceaf2c3f8d53b07b449ed8d1180c
Alina <alina.kirov@mail.ru>
2022-10-26 20:59:03 +0300

    готово

---
+++ js/script.js
@@ -0,0 +1,66 @@
+const todoControl = document.querySelector(".todo-control");
+const headerInput = document.querySelector(".header-input");
+const todoList = document.querySelector(".todo-list");
+const todoCompleted = document.querySelector(".todo-completed");
+
+const toDoData = [];
+
+const render = function () {
+  todoList.innerHTML = "";
+  todoCompleted.innerHTML = "";
+
+  toDoData.forEach(function (item) {
+    const li = document.createElement("li");
+
+    li.classList.add("todo-item");
+
+    li.innerHTML =
+      '<span class="text-todo">' +
+      item.text +
+      "</span>" +
+      '<div class="todo-buttons">' +
+      '<button class="todo-remove"></button>' +
+      '<button class="todo-complete"></button>' +
+      "</div>";
+
+    if (item.completed) {
+      todoCompleted.append(li);
+    } else {
+      todoList.append(li);
+    }
+
+    li.querySelector(".todo-complete").addEventListener("click", function () {
+      item.completed = !item.completed;
+      render();
+    });
+
+    li.querySelector(".todo-remove").addEventListener("click", function () {
+      li.remove();
+      item.completed = false;
+    });
+  });
+};
+
+todoControl.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const newToDo = {
+    text: headerInput.value,
+    completed: false,
+  };
+
+  if (headerInput.value === "") {
+    alert("Введите текст");
+  } else {
+    toDoData.push(newToDo);
+  }
+
+  localStorage.setItem('newToDo', JSON.stringify(newToDo));
+
+  const retrievedObject = localStorage.getItem('newToDo');
+  'retrievedObject: ', JSON.parse(retrievedObject);
+
+  headerInput.value = "";
+
+  render();
+});
+
